dbc6ebf6815757b5ec988232b65de2583dd47417
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-02 12:17:26 +0530

    DAY 1

--- day1.java
+++ day1.java
@@ -19,14 +19,46 @@ public class day1{
         
         //decision making statement
         //age based voting checker
-        int age;
-        age=sc.nextInt();
-        if(age>=18)
-        {
-            System.out.println("Eligible for Vote");
+        // int age;
+        // age=sc.nextInt();
+        // if(age>=18)
+        // {
+        //     System.out.println("Eligible for Vote");
+        // }
+        // else{
+        //     System.out.println("Not Eligible for vote");
+        // }
+        //Even odd number checker
+        // int number;
+        // number=sc.nextInt();
+        // if(number%2==0){
+        //     System.out.println("Even Number");
+        // }
+        // else{
+        //     System.out.println("Odd Number");
+        // }
+        // int number;
+        // number=sc.nextInt();
+        // if(number%3==0 && number%5==0)
+        // {
+        //     System.out.println("Divisible by 3 and 5");
+        // }
+        // else if(number%5==0)
+        // {
+        //     System.out.println("Divisible by 5");
+        // }
+        // else if(number%3==0){
+        //     System.out.println("Divisible by 3");
+        // }
+    
+    
+        char ch=sc.next().charAt(0);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            System.out.println("Vowel");
         }
         else{
-            System.out.println("Not Eligible for vote");
+            System.out.println("Consonent");
         }
+    }   
     }
-}
+

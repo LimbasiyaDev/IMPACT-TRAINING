4b9297eb529f4a4c371126f389ba12f1ec00068c
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-02 11:40:56 +0530

    DAY 1

--- day1.java
+++ day1.java
@@ -1,6 +1,32 @@
-import java.util.*;
-class day1{
+// import java.util.*;
+// class day1{
+//     public static void main(String[] args){
+//         System.out.println("Hello");
+//     }
+// }
+
+import java.util.Scanner;
+public class day1{
     public static void main(String[] args){
-        System.out.println("Hello");
+        // int a;
+        // a=10;
+        // System.out.println(a);
+         Scanner sc=new Scanner(System.in);
+        // int b=sc.nextInt();
+        // double x=sc.nextDouble();
+        // System.out.println(x);
+        // System.out.println(b);
+        
+        //decision making statement
+        //age based voting checker
+        int age;
+        age=sc.nextInt();
+        if(age>=18)
+        {
+            System.out.println("Eligible for Vote");
+        }
+        else{
+            System.out.println("Not Eligible for vote");
+        }
     }
 }

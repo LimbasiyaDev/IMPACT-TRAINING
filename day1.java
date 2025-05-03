f3a4d6ef913a21f7f028d59c56262e5b3e468192
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-03 10:43:36 +0530

    DAY 2

--- day1.java
+++ day1.java
@@ -6,60 +6,58 @@
 // }
 
 import java.util.Scanner;
-public class day1{
-    public static void main(String[] args){
+
+public class day1 {
+    public static void main(String[] args) {
         // int a;
         // a=10;
         // System.out.println(a);
-         Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         // int b=sc.nextInt();
         // double x=sc.nextDouble();
         // System.out.println(x);
         // System.out.println(b);
-        
-        //decision making statement
-        //age based voting checker
+
+        // decision making statement
+        // age based voting checker
         // int age;
         // age=sc.nextInt();
         // if(age>=18)
         // {
-        //     System.out.println("Eligible for Vote");
+        // System.out.println("Eligible for Vote");
         // }
         // else{
-        //     System.out.println("Not Eligible for vote");
+        // System.out.println("Not Eligible for vote");
         // }
-        //Even odd number checker
+        // Even odd number checker
         // int number;
         // number=sc.nextInt();
         // if(number%2==0){
-        //     System.out.println("Even Number");
+        // System.out.println("Even Number");
         // }
         // else{
-        //     System.out.println("Odd Number");
+        // System.out.println("Odd Number");
         // }
         // int number;
         // number=sc.nextInt();
         // if(number%3==0 && number%5==0)
         // {
-        //     System.out.println("Divisible by 3 and 5");
+        // System.out.println("Divisible by 3 and 5");
         // }
         // else if(number%5==0)
         // {
-        //     System.out.println("Divisible by 5");
+        // System.out.println("Divisible by 5");
         // }
         // else if(number%3==0){
-        //     System.out.println("Divisible by 3");
+        // System.out.println("Divisible by 3");
         // }
-    
-    
-        char ch=sc.next().charAt(0);
-        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+
+        char ch = sc.next().charAt(0);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
             System.out.println("Vowel");
-        }
-        else{
+        } else {
             System.out.println("Consonent");
         }
-        
-    }   
-    }
 
+    }
+}
--- day2.java
+++ day2.java
@@ -26,12 +26,61 @@
     
 // }
 
+// public class day2 {
+//     public static void main(String[] args) {
+//         int x=5;
+//         int y=++x;
+//         int z=x++;
+//         System.out.println(x+" "+y+" "+z);
+//     }
+    
+// }
+
+// public class day2 {
+//     public static void main(String[] args) {
+//         int x=5;
+//         int y=x++;
+//         int z=x--;
+//         System.out.println(x+" "+y+" "+z);
+//     }
+    
+// }
+
+//Bitwise OR
+
+// public class day2 {
+//     public static void main(String[] args) {
+//         int x=(5|2);
+//         System.out.println(x);
+//     }
+    
+// }
+
+//Bitwise AND
+
+// public class day2 {
+//     public static void main(String[] args) {
+//         int x=(4&13);
+//         System.out.println(x);
+//     }
+    
+// }
+
+// Bitwise XOR
+
+// public class day2 {
+//     public static void main(String[] args) {
+//         int x=(8^16);
+//         System.out.println(x);
+//     }
+    
+// }
+
+//Operators
 public class day2 {
     public static void main(String[] args) {
-        int x=5;
-        int y=++x;
-        int z=x++;
-        System.out.println(x+" "+y+" "+z);
+        boolean x=(5>4)&&(4<7);
+        System.out.println(x);
     }
     
 }

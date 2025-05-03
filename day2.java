58c01c77c902bf2f2e1cba4e2723e626315412ff
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-03 11:41:16 +0530

    DAY 2

--- day2.java
+++ day2.java
@@ -77,10 +77,61 @@
 // }
 
 //Operators
+// public class day2 {
+//     public static void main(String[] args) {
+//         boolean x=(5>4)&&(4<7);
+//         System.out.println(x);
+//     }
+    
+// }
+
+// import java.util.Scanner;
+// public class day2 {
+//     public static void main(String[] args) {
+     
+//         Scanner sc = new Scanner(System.in);
+//         int x=sc.nextInt();
+//         if(x>=33 && x<45){
+//             System.out.println("3rd Division");
+//         }
+//         else if(x>=60){
+//             System.out.println("2nd Division");
+//         }
+//         else if(x<33){
+//             System.out.println("Fail");
+//         }
+//         else if(x>=33){
+//             System.out.println("Pass");
+//         }       
+//     }
+// }
+
+
+
+
+
+import java.util.Scanner;
 public class day2 {
     public static void main(String[] args) {
-        boolean x=(5>4)&&(4<7);
-        System.out.println(x);
+     
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Amount Of Purchase");
+        double x=sc.nextInt();
+        if(x>0 && x<1000){
+            System.out.println("5% Discount");
+            System.out.println("Amount to Pay "+(x-(x*5/100)));
+        }
+        else if(x>1000 && x<5000){
+            System.out.println("10% Discount");
+            System.out.println("Amount to Pay "+(x-(x*10/100)));
+        }
+        else if(x>5000 && x<10000){
+            System.out.println("15% Discount");
+            System.out.println("Amount to Pay "+(x-(x*15/100)));
+        }
+        else if(x>10000){
+            System.out.println("20% Discount");
+            System.out.println("Amount to Pay "+(x-(x*20/100)));
+        } 
     }
-    
 }

38f3905c94efd63182a7fb0856ea44883c739a17
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-03 10:13:13 +0530

    DAY 2

--- day2.java
+++ day2.java
@@ -1,7 +1,37 @@
+// public class day2 {
+//     public static void main(String[] args) {
+//         double x=5.0%2;
+//         System.out.println(x);
+//     }
+    
+// }
+
+// Increment and decrement operator
+
+// public class day2 {
+//     public static void main(String[] args) {
+//         int x=5;
+//         int y=x++;
+//         System.out.println(x+" "+y);
+//     }
+    
+// }
+
+// public class day2 {
+//     public static void main(String[] args) {
+//         int x=5;
+//         int y=++x;
+//         System.out.println(x+" "+y);
+//     }
+    
+// }
+
 public class day2 {
     public static void main(String[] args) {
-        double x=5.0/2;
-        System.out.println(x);
+        int x=5;
+        int y=++x;
+        int z=x++;
+        System.out.println(x+" "+y+" "+z);
     }
     
 }

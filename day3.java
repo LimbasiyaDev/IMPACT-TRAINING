6ea6f83e566369e71d1af43346d8b012d793b203
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-05 09:41:00 +0530

    DAY 3

--- day3.java
+++ day3.java
@@ -1,13 +1,89 @@
+// import java.util.Scanner;
+
+// public class day3 {
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n=sc.nextInt();
+//     int count = 0;
+//     for(int i=0; i<=n; i++){
+//     count = count + i;
+//     }
+//     System.out.println(count);
+// }  
+// }
+
+// import java.util.Scanner;
+
+// public class day3 {
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int number,count = 0;
+//     number=sc.nextInt();
+//     while(number>0)
+//     {
+//         int x = number%10;
+//         System.out.println(x);
+//         number/=10;
+//     }
+// }
+// }
+
+//Armstrong Number
+// import java.util.Scanner;
+
+// public class day3{
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter Number");
+//     int number = sc.nextInt();
+//     int duplicate=number;
+//     int count = 0;
+//     while(number>0) 
+//     {
+//         count ++;
+//         number/=10;
+//     }
+//     number=duplicate;
+//     int z=0;
+//     while(number>0)
+//     {
+//         int x = number%10;
+//         int y = 1;
+//         for(int i=1;i<count;i++)
+//         {
+//             y=y*x;
+//         }
+//         z+=y;
+//         number /= 10;
+//     }
+// if(z==duplicate){
+//     System.out.println("Armstrong");
+// }
+// else{
+//     System.out.println("Not");
+// }
+// }
+// }
+
+
+
+//Pattern Print 
 import java.util.Scanner;
 
-public class day3 {
+public class day3{
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n=sc.nextInt();
-    int count = 0;
-    for(int i=0; i<=n; i++){
-    count = count + i;
+    int n = sc.nextInt();
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+        System.out.print('*');
+        }
+        System.out.println();
     }
-    System.out.println(count);
-}  
+    }
 }
+
+

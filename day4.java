c9d8273140d1e820cec76633fe6109f766ec65b4
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-06 10:23:22 +0530

    DAY 4

---
+++ day4.java
@@ -0,0 +1,68 @@
+//NUMBER 0,1 PATTERN
+// import java.util.Scanner;
+// public class day4 {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++)
+//             {
+//             if((i+j)%2==0){
+//             System.out.print("1");}
+//             else{
+//             System.out.print("0");}
+//             } 
+//             System.out.println();  
+//         }
+//     }
+// }
+
+//NUMBER PATTERN
+// import java.util.Scanner;
+
+// public class day4{
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int number = 1;
+//         for(int i=1;i<=n;i++)
+//         {
+//         for(int j=1;j<=i;j++)
+//         {
+//         System.out.print(number+" ");
+//         number++;
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
+
+
+
+import java.util.Scanner;
+
+public class day4{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 || i==n-1 || j==0 || j==n-1){
+            System.out.print('*');
+            }
+            else{
+            System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+    }
+}
+
+
+    
+
+
+    
+

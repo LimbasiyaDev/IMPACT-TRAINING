eb111ee00c504fcfb023235f9acf13b2c573d6df
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-06 10:23:57 +0530

    DAY 4

--- day4.java
+++ day4.java
@@ -18,47 +18,47 @@
 // }
 
 //NUMBER PATTERN
-// import java.util.Scanner;
-
-// public class day4{
-//     public static void main(String[] args) {
-//     Scanner sc = new Scanner(System.in);
-//     int n = sc.nextInt();
-//     int number = 1;
-//         for(int i=1;i<=n;i++)
-//         {
-//         for(int j=1;j<=i;j++)
-//         {
-//         System.out.print(number+" ");
-//         number++;
-//         }
-//         System.out.println();
-//     }
-//     }
-// }
-
-
-
-
 import java.util.Scanner;
 
 public class day4{
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==0 || i==n-1 || j==0 || j==n-1){
-            System.out.print('*');
-            }
-            else{
-            System.out.print(" ");
-            }
+    int number = 1;
+        for(int i=1;i<=n;i++)
+        {
+        for(int j=1;j<=i;j++)
+        {
+        System.out.print(number+" ");
+        number++;
         }
         System.out.println();
     }
     }
 }
+
+
+
+
+// import java.util.Scanner;
+
+// public class day4{
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             if(i==0 || i==n-1 || j==0 || j==n-1){
+//             System.out.print('*');
+//             }
+//             else{
+//             System.out.print(" ");
+//             }
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
 
 
     

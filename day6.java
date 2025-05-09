2554e3e650a82b61b3a81e1ae05eec3ea0f20db0
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-09 09:56:36 +0530

    DAY 6

--- day6.java
+++ day6.java
@@ -40,38 +40,56 @@
 
 
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class day6 {
-    public static void main(String[] args) {
-        int arr[];
-        int target = 8;
-        Scanner sc = new Scanner(System.in);
-        int n;
-        n = sc.nextInt();
-        arr = new int[n];
+// public class day6 {
+//     public static void main(String[] args) {
+//         int arr[];
+//         int target = 8;
+//         Scanner sc = new Scanner(System.in);
+//         int n;
+//         n = sc.nextInt();
+//         arr = new int[n];
 
      
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
 
-        int first = -1;
-        int last = -1;
+//         int first = -1;
+//         int last = -1;
 
        
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == target) {
-                if(first==-1){
-                first = i;
-                last = i;
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] == target) {
+//                 if(first==-1){
+//                 first = i;
+//                 last = i;
+//             }
+//             else{
+//                 last = i;
+//             }
+//         }
+//             }
+//         System.out.println("First Occurrence: " + first);
+//         System.out.println("Last Occurrence: " + last);
+//     }
+// }
+
+
+
+public class day6 {
+        public static void main(String[] args){
+            int arr[][]={{1,2,3},{3,5,6}};
+            int column=arr[0].length;
+            int row=arr.length;
+            System.out.println(row+" "+column);
+            for(int i=0;i<row;i++){
+                for(int j=0;j<column;j++){
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
             }
-            else{
-                last = i;
-            }
+
         }
-            }
-        System.out.println("First Occurrence: " + first);
-        System.out.println("Last Occurrence: " + last);
-    }
 }

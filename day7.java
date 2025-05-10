0efc155cd32bc9b668d76465cc311ca5ffb6a4e5
LimbasiyaDev <limbasiyadev232004@gmail.com>
2025-05-10 07:48:18 +0530

    DAY 7

---
+++ day7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class day7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr [][];
+        arr = new int[3][];
+        arr[0] = new int[3];
+        arr[1] = new int[2];
+        arr[2] = new int[3];
+    }
+}
